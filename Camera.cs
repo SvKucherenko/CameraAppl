68245b0d3fe7f543819c8035dde0e81961555e1b
SvKucherenko <ryabkov@bk.ru>
2014-08-25 06:23:11 +0400

    first commit

---
+++ Camera.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+using EDSDKLib;
+
+
+namespace CameraDLL
+{
+     
+    public class CameraDLL
+    {
+        public event EventHandler<EventArgs> PictureTaked;
+        public event EventHandler<LivePictureUpdatedEventArgs> LivePictureUpdated;
+        SDKHandler CameraHandler;
+     List<int> AvList;
+          List<int> TvList;
+        List<int> ISOList;
+        List<Camera> CamList;
+        Bitmap Evf_Bmp;
+        
+        
+        string SavePathText = "";
+        protected virtual void OnPictureTaked(EventArgs e)
+        {
+            EventHandler<EventArgs> handler = PictureTaked;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        protected virtual void OnLivePictureUpdated(LivePictureUpdatedEventArgs e)
+        {
+            EventHandler<LivePictureUpdatedEventArgs> handler = LivePictureUpdated;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        public bool IsLiveViewOn()
+        {
+            if (CameraHandler == null) return false;
+            return (CameraHandler.IsLiveViewOn);
+        }
+        public string ImageSaveDirectory
+        {
+            get   
+            {
+                if (CameraHandler != null)
+                    return (CameraHandler.ImageSaveDirectory);
+                else return null;
+            }
+            set 
+            {
+                if (CameraHandler != null)
+                {
+                    CameraHandler.ImageSaveDirectory = value;
+                    if (!Directory.Exists(SavePathText)) Directory.CreateDirectory(SavePathText);
+                }
+            }
+        }
+        public bool Start()
+        {
+            try
+            {
+                CameraHandler = new SDKHandler();
+                
+
+              
+                CameraHandler.PictureTaked+=new SDKHandler.PictureTakedHandler(CameraHandler_PictureTaked);
+                CameraHandler.LiveViewUpdated += new SDKHandler.StreamUpdate(CameraHandler_LiveViewUpdated);
+                SavePathText = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "RemotePhoto");
+                if (!Directory.Exists(SavePathText)) Directory.CreateDirectory(SavePathText);
+                CamList = CameraHandler.GetCameraList();
+                if (CamList.Count != 0)
+                {
+                    CameraHandler.OpenSession(CamList[0]);
+                    CameraHandler.ImageSaveDirectory = SavePathText;
+                    CameraHandler.SetSetting(EDSDK.PropID_SaveTo, (uint)EDSDK.EdsSaveTo.Host);
+                    CameraHandler.SetSetting(EDSDK.PropID_ImageQuality, (uint)EDSDK.ImageQuality.EdsImageQuality_LR);
+                    CameraHandler.SetCapacity();
+                }
+                else return false; // no camera
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        void CameraHandler_LiveViewUpdated(Stream img)
+        {
+            LivePictureUpdatedEventArgs e= new LivePictureUpdatedEventArgs();
+            e.img=img;
+            OnLivePictureUpdated(e);
+        }
+
+        void CameraHandler_PictureTaked()
+        {
+            lock (CameraHandler)
+            {
+                OnPictureTaked(null);
+            }
+        }
+
+        public List<string> GetAvList()
+        {
+            if (CameraHandler == null) return null;
+            List<string> ret = new List<string>();
+            foreach (int Av in CameraHandler.GetSettingsList((uint)EDSDK.PropID_Av))
+            {
+                ret.Add(CameraValues.AV((uint)Av));
+            }
+            return ret;
+        }
+
+        public List<string> GetTvList()
+        {
+            if (CameraHandler == null) return null;
+            List<string> ret = new List<string>();
+            foreach (int Tv in CameraHandler.GetSettingsList((uint)EDSDK.PropID_Tv))
+            {
+                ret.Add(CameraValues.TV((uint)Tv));
+            }
+            return ret;
+        }
+        public void SetAV(string Av)
+        {
+            if (CameraHandler != null)
+            CameraHandler.SetSetting(EDSDK.PropID_Av, CameraValues.AV((string)Av));
+        }
+        public void SetTV(string Tv)
+        {
+            if (CameraHandler != null)
+            CameraHandler.SetSetting(EDSDK.PropID_Tv, CameraValues.TV((string)Tv));
+        }
+        
+        public void Stop()
+        {
+            if (CameraHandler != null)
+            CameraHandler.Dispose();
+        }
+        public void StartLiveView()
+        {
+            if (CameraHandler != null)
+                CameraHandler.StartLiveView();
+        }
+        public void StopLiveView()
+        {
+            if (CameraHandler != null)
+                CameraHandler.StopLiveView();
+        }
+        public void TakePhoto()
+        {
+            if (CameraHandler != null)
+                CameraHandler.TakePhoto();
+        }
+
+    }
+    public class LivePictureUpdatedEventArgs : EventArgs
+    {
+        public Stream img { get; set; }
+        
+    }
+}
---
+++ EOSApp/EOSApp/MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Threading;
+using System.Windows.Threading;
+using System.Windows.Forms;
+
+namespace EOSApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CameraDLL.CameraDLL cam = new CameraDLL.CameraDLL();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        bool bDisconnecting = false;
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnConnect.Content.ToString() == "Connect")
+            {
+                txtSavePatchTextBox.Text = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "RemotePhoto").ToString();
+                if (cam.Start())
+                {
+                    btnConnect.Content = "Disconnect";
+                    List<string> l = cam.GetAvList();
+                    foreach (string c in l)
+                    {
+                        cboAppertures.Items.Add(c);
+                    }
+                    l = cam.GetTvList();
+                    foreach (string c in l)
+                    {
+                        cboShutterTimes.Items.Add(c);
+                    }
+                    cam.LivePictureUpdated += new EventHandler<CameraDLL.LivePictureUpdatedEventArgs>(cam_LivePictureUpdated);
+                    cam.PictureTaked += new EventHandler<EventArgs>(cam_PictureTaked);
+                }
+            }
+            else
+            {
+                bDisconnecting = true;
+                cboAppertures.Items.Clear();
+                cboShutterTimes.Items.Clear();
+                cam.Stop();
+               
+                btnConnect.Content = "Connect";
+                bDisconnecting = false;
+            }
+        }
+
+        void cam_PictureTaked(object sender, EventArgs e)
+        {
+            System.Windows.MessageBox.Show("Picture taked");
+        }
+        private  void SetBitmap(BitmapImage b)
+        {
+            imgPicture.Source = b;
+        }
+
+        void cam_LivePictureUpdated(object sender, CameraDLL.LivePictureUpdatedEventArgs e)
+        {
+            using (var stream = e.img)
+            {
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.StreamSource = stream;
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
+                bitmap.Freeze();
+
+                Thread t = new Thread(new ThreadStart(
+        delegate
+        {
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action<BitmapImage>(SetBitmap), bitmap);
+        }
+        ));
+                t.Start();
+                
+            }
+        }
+
+        private void cboAppertures_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!bDisconnecting)
+            cam.SetAV(cboAppertures.SelectedItem.ToString());
+        }
+
+        private void cboShutterTimes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!bDisconnecting)
+            cam.SetTV(cboShutterTimes.SelectedItem.ToString());
+        }
+
+        private void btnLiveView_Click(object sender, RoutedEventArgs e)
+        {
+            if (!cam.IsLiveViewOn())
+            { btnLiveView.Content = "Stop LV";
+            cam.StartLiveView();
+            }
+            else
+            { btnLiveView.Content = "Start LV";
+            cam.StopLiveView();
+            }
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+               System.Windows.Forms.FolderBrowserDialog SaveFolderBrowser= new System.Windows.Forms.FolderBrowserDialog() ;
+               if (SaveFolderBrowser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+               {
+                   txtSavePatchTextBox.Text = SaveFolderBrowser.SelectedPath;
+                   cam.ImageSaveDirectory = SaveFolderBrowser.SelectedPath;
+               }
+        }
+
+        private void btnTakePicture_Click(object sender, RoutedEventArgs e)
+        {
+            cam.TakePhoto();
+        }
+    }
+}
